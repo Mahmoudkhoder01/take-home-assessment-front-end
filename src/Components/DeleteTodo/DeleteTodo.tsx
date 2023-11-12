a02624483326906320a784111d13cee2f65678fc
Mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2023-11-12 17:42:13 +0200

    create the deleteTodo component with full functionnality

--- src/Components/DeleteTodo/DeleteTodo.tsx
+++ src/Components/DeleteTodo/DeleteTodo.tsx
@@ -1,5 +1,59 @@
+import React, { FC, useEffect, useState } from "react";
+import Box from "../Box/Box";
+import Button from "../ReusableTools/Button/Button";
 import classes from "./DeleteTodo.module.css";
+import closeIcon from "../../ICONS/x-mark.png";
+import { deleteTodo } from "../../API/TODOAPI";
 
-export default function DeleteTodo() {
-  return <div>DeleteTodo</div>;
+interface DeleteTodoProps {
+  setIsDeleteTodo: (value: boolean) => void;
+  reFetch: () => void;
+  todoId: number;
 }
+
+const DeleteTodo: FC<DeleteTodoProps> = ({
+  setIsDeleteTodo,
+  todoId,
+  reFetch,
+}) => {
+  const [currentTodoId, setCurrentTodoId] = useState(todoId);
+
+  useEffect(() => {
+    // Update the currentTodoId whenever todoId prop changes
+    setCurrentTodoId(todoId);
+  }, [todoId]);
+
+  const handleClose = () => {
+    setIsDeleteTodo(false);
+  };
+
+  const handleDeleteTodo = async () => {
+    setIsDeleteTodo(false);
+    await deleteTodo(currentTodoId);
+    reFetch();
+  };
+
+  return (
+    <>
+      <img
+        src={closeIcon}
+        alt="close-icon"
+        className={classes.closeIcon}
+        onClick={handleClose}
+      />
+      <div className={classes.backDrop}>
+        <Box
+          content={
+            <>
+              <p>Are you sure you want to delete this todo?</p>
+              <Button text="Yes" onClick={handleDeleteTodo} />
+              <Button text="No" onClick={handleClose} />
+            </>
+          }
+        />
+      </div>
+    </>
+  );
+};
+
+export default DeleteTodo;
