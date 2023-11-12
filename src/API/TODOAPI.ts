f0baaeb85fbadd5194a79de68bcfe764d95372fd
Mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2023-11-12 12:54:34 +0200

    add the todo api for creating the todo

--- src/API/TODOAPI.ts
+++ src/API/TODOAPI.ts
@@ -22,3 +22,44 @@ export const fetchTodos = async (): Promise<Todo[]> => {
   const response = await axios.get<Todo[]>(`${API_URL}todo`);
   return response.data;
 };
+
+export interface CreateTodoResponse {
+  message: string;
+  result: {
+    id: number;
+    description: string;
+    priority: string;
+    userId: number;
+    date: string;
+    completed: false;
+    createdAt: string;
+    updatedAt: string;
+  };
+}
+
+export interface CreateTodoRequest {
+  description: string;
+  priority: string;
+  userId: number;
+  date: string;
+  completed?: boolean;
+}
+
+export const createTodo = async (
+  todo: CreateTodoRequest
+): Promise<CreateTodoResponse> => {
+  try {
+    const response = await axios.post<CreateTodoResponse>(
+      `${API_URL}todo`,
+      todo
+    );
+
+    return response.data;
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.message);
+    } else {
+      throw new Error("Failed to craete todo");
+    }
+  }
+};
